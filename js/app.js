420212b66b0a8aaf78a266a6f3e424deecefd40b
Dan Riding <doonyby@gmail.com>
2016-04-14 11:16:47 -0600

    non-interactive complete

--- js/app.js
+++ js/app.js
@@ -4,32 +4,32 @@ var quiz = {
 		{
 			question: "What artificial trigger is responsible for causing the most avalanches in backcountry terrain?",
 			answers: ["Snowmobiles", "Skiers", "Snowboarders", "Hikers"],
-			correct: 0
-			explanation: "Snowmobiles are responsible for triggering the most artificial avalanches of all backcountry recreators.  This is partially because of how much ground they can cover in a short period of time."
+			correct: 0,
+			explanation: "Snowmobiles are responsible for triggering the most artificial avalanches of all backcountry recreators.  This is partially because of how much ground they can cover in a short period of time.",
 		},
 		{
 			question: "What type of snow is not considered a persistent weak layer in snow pack?",
 			answers: ["Facets", "Burried Surface Hoar", "Wind Slab", "Depth Hoar"],
-			correct: 2
-			explanation: "Wind slab is not considered a persistent weak layer, although it is common for a wind slab to sit on top of a persistent weak layer.  Therin lies the avalanche danger."
+			correct: 2,
+			explanation: "Wind slab is not considered a persistent weak layer, although it is common for a wind slab to sit on top of a persistent weak layer.  Therin lies the avalanche danger.",
 		},
 		{
 			question: "What slope degree should you start worrying about possible avalanche danger?",
 			answers: ["20", "30", "40", "45"],
-			correct: 1
-			explanation: "Usually, slopes less than 30 degrees are not a danger when assessing for avalanches.  The typical window for slop angle avalanche danger falls between 30 and 40 degrees."
+			correct: 1,
+			explanation: "Usually, slopes less than 30 degrees are not a danger when assessing for avalanches.  The typical window for slop angle avalanche danger falls between 30 and 40 degrees.",
 		},
 		{
 			question: "What factor is not included when testing for snow stability with an avalanche pit test?",
 			answers: ["Strength", "Energy", "Structure", "Terrain"],
-			correct: 3
-			explanation: "Pit tests are specifically run to test the strength, energy, and structure of a snow pack.  Terrain should be continually evaluated throughout backcountry travel."
+			correct: 3,
+			explanation: "Pit tests are specifically run to test the strength, energy, and structure of a snow pack.  Terrain should be continually evaluated throughout backcountry travel.",
 		},
 		{
 			question: "What of the following is considered a terrain trap in an avalanche?",
 			answers: ["Trees", "Rocks", "Gullies", "All of the above"],
-			correct: 3
-			explanation: "Trees, rocks, gullies and cliffs can all be dangerous terrain traps when caught in an avalanche.  If possible, choose slopes that have a gentle run out without many terrain traps."
+			correct: 3,
+			explanation: "Trees, rocks, gullies and cliffs can all be dangerous terrain traps when caught in an avalanche.  If possible, choose slopes that have a gentle run out without many terrain traps.",
 		},
 	]
 }
@@ -50,7 +50,7 @@ $(document).ready(function() {
 		$('.score-page').hide();
 	});
 
-	$('.quiz-page').click(function() {
+	$('input').click(function() {
 		console.log("I'm a score page");
 		$('.title-page').hide();
 		$('.quiz-page').hide();
